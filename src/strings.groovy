5b997e412333b3a16f5fb9cc91b3cb4ecdbf41bf
uf <uniformfoxtrot@icloud.com>
2020-05-28 20:50:25 +0530

    Initial Code

---
+++ src/strings.groovy
@@ -0,0 +1,35 @@
+class strings {
+    static void main(String[] args) {
+        def name = "Derek";
+        println('Iam ${name}\n');
+        println("Iam ${name}\n");
+        println("Iam $name\n");
+        def multistring = '''nsdhfsjkdh
+            fdsdfhsdkjf
+            fsdklfskdfjdskl''';
+        println(multistring);
+        println("3 rd index of name-->"+ name[3]);
+        println("location of r-->"+name.indexOf('r'));
+        println("0-2 location-->" + name[0..2]);
+        println("Every other character "+ name[0,2,4]);
+        println(name.substring(1));
+        println(name.substring(1,4));
+
+        println("My anme " +name);
+        println("My name".concat("new"));
+        println("My name is "*2);
+        println("equating strings "+ ('Derek'.equals('Derek')));
+        println("equating strings "+ ('Derek'.equalsIgnoreCase('derek')));
+        println("Length ="+name.length());
+
+
+        def repeatStr = " what i said is true" * 2;
+        println(repeatStr - "what");
+
+        println(repeatStr.split(' '));
+        println(repeatStr.toList());
+    }
+}
+
+
+
